01cbdbd952f8a8badb8d50759cc15231158a97c1
KhariMaurice <kharimauriceschool@gmail.com>
2018-06-26 11:35:46 -0400

    Added argument "a1a1b1b2" and if statement to give LikelyData more flexibility

--- Weakest_link/R/Likelydata.R
+++ Weakest_link/R/Likelydata.R
@@ -1,5 +1,11 @@
 Likelydata <-
-function(data = temp, a1 = 1, a2 = 2, b1 = 1, b2 = 2){
+function(data = temp, a1 = 1, a2 = 2, b1 = 1, b2 = 2, a1a2b1b2){
+  if(!missing(a1a2b1b2)){
+    a1 = a1a2b1b2$a1
+    a2 = a1a2b1b2$a2
+    b1 = a1a2b1b2$b1
+    b2 = a1a2b1b2$b2
+  }
   phij1 = inverselogit(a1 + b1 * data$x1)
   phij2 = inverselogit(a2 + b2 * data$x2)
   phij1phij2 = cbind(phij1, phij2)
