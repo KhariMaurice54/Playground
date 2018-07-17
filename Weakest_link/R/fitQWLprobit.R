aab877618ea14eff87406e65ab67dfca2845902c
Roger Day <notsofeo@gmail.com>
2018-07-17 13:28:59 -0400

    fitQWLprobit handles fitting of delta and a vector of deltas. In progress: Trying to set it up to handle survival data other non-binary data.

--- Weakest_link/R/fitQWLprobit.R
+++ Weakest_link/R/fitQWLprobit.R
@@ -1,26 +1,68 @@
 #### fitQWLprobit:  fit a parametric weakest link model ####
-fitQWLprobit = function(data, delta=0, dir1 = TRUE, dir2 = TRUE, 
-                  testMe = FALSE, plotMe = TRUE,
-                  ...) {
+
+normcdf = function(x) pnorm(x, mean(x), sd(x))
+## comparing normcdf to ecdf:
+p45x1 = mb[[ p45[1] ]]
+plot( ecdf(x = p45x1) )
+lines(x=qnorm(
+  Ptemp<-seq(0,1,length=length(p45x1))
+), y=Ptemp, col='green', lwd=3)
+
+fitDelta = function(delta, data = data) {
+  fitQWLprobit(data = data, testMe = FALSE,
+               plotData = FALSE, delta = delta 
+  )$aic
+}
+
+fitQWLprobit = function(data, endpoint='y',
+                        delta, 
+                        dir1 = TRUE, dir2 = TRUE, 
+                        testMe = FALSE, plotData = TRUE,
+                        ...) {
   if(testMe)
     data = WLContinuousdata(...)
-  attach(data)
-  on.exit(detach())
+  x1 = data$x1
+  x2 = data$x2
+  y = data$y
   Fhat1 = pnorm(x1, mean(x1), sd(x1)) * ifelse(dir1, 1, -1)
   Fhat2 = pnorm(x2, mean(x2), sd(x2)) * ifelse(dir2, 1, -1)
-  if(delta == 0) {
-    phi2 = Fhat2
-  } else {
-    H = inverselogit
-    Hinv= logit
-    phi2 = 1 - H(Hinv(1-Fhat2) -  delta)
+  H = inverselogit
+  Hinv= logit
+  fitOneDelta = function(delta) {
+    if(delta == 0) {
+      phi2 <<- Fhat2
+    } else if(length(delta)==1) {
+      phi2 <<- 1 - H(Hinv(1-Fhat2) -  delta)
+    }
+    phiMin = pmin(Fhat1, phi2)
+    if(endpoint == 'surv') {
+      ySurv = attr(data, 'ySurv')
+      require(survival)
+      result = coxph(ySurv ~ phiMin)
+    }
+    else {
+      if(all(data[[endpoint]] %in% c(0,1,NA) ) ) 
+        fam = binomial
+      else 
+        fam = normal
+      result  = glm(y ~ phiMin, family=binomial, data=data)
+    }
   }
-  phiMin = pmin(Fhat1, phi2)
-  result  = glm(y ~ phiMin)
-  if(plotMe) {
+  if(length(delta) == 1)
+    result = fitOneDelta(delta)
+  else {
+    deltaInterval = ifelse(missing(delta),
+                           c(-3,3), delta)
+    result = optimize(fitDelta, 
+                      interval = deltaInterval, 
+                      tol = 1e-3)
+  }  
+  
+  if(plotData) {
     plot(Fhat2, phi2)
-    plot(x1, x2, pch=c('0','1')[y+1], 
-         col=c('red','green')[y+1])
+    colorChoice = 1 + (y > median(y))
+    plot(x1, x2, pch=c('0','1')[colorChoice], 
+         col=c('red','green')[colorChoice])
     # COU is where phi1 = phi2, Fhat1 = phi2,
     #  But phi2 = 1 - H(Hinv(1-Fhat2) -  delta),
     # so Fhat2 = 1 - H(Hinv(1 - Fhat1) + delta)
@@ -28,7 +70,18 @@ fitQWLprobit = function(data, delta=0, dir1 = TRUE, dir2 = TRUE,
     matching_x2 = qnorm(matching_P2, mean=mean(x2), sd=sd(x2))
     lines(x1[order(x1)], matching_x2[order(x1)] )
   }
-  summary(result)
+  return(summary(result) )
 }
-fitQWLprobit(testMe=TRUE, plotMe = TRUE, delta=2, 
-       b1 = 3, b2 = 5)
+
+fitQWLprobit(testMe=TRUE, plotData = TRUE, delta=1e-15, 
+             b1 = 3, b2 = 5)
+testData = WLContinuousdata( b1 = 3, b2 = 5)
+
+deltaSeq = seq(-3,3,length=100)
+resultSeq = sapply(deltaSeq, fitDelta, data=testData)
+plot(deltaSeq, resultSeq, xlab='delta', ylab='AIC')
+
+optResult = optimize(fitDelta, data=testData,
+                     interval = c(-3,3), tol = 1e-3)
+abline(v=optResult$minimum, h=optResult$objective, col='red')
+
