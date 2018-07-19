d09861f556e87ad88e7980d9d83926ad01408c0e
Roger Day <notsofeo@gmail.com>
2018-07-19 17:25:56 -0400

    Documentation for logit and inverselogit

--- Weakest_link/R/inverselogit.R
+++ Weakest_link/R/inverselogit.R
@@ -1,5 +1,15 @@
-inverselogit <-
-function(z){
-  exp(z)/ (1 + exp(z))
-}
-logit = function(p) log(p/(1-p))
+#' logit and inverselogit
+#' 
+#' These functions have distinct uses:
+#'  (1) in defining phi for WLContinuousdata models, and
+#'  (2) for H and Hinv in the quantile stitching models.
+#'  
+#'  @param z A value in (-Inf, Inf)
+#'  @param p A value in [0,1]
+
+H <- inverselogit <-
+  function(z){
+    exp(z)/ (1 + exp(z))
+  }
+
+Hinv <- logit = function(p) log(p/(1-p))
