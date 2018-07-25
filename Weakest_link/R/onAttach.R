8c4b64d546f9a05282e71f6e1c0957be4455e675
Roger Day <notsofeo@gmail.com>
2018-07-25 12:05:45 -0400

    Make sure that D7 is saved.

--- Weakest_link/R/onAttach.R
+++ Weakest_link/R/onAttach.R
@@ -2,8 +2,8 @@
   data(p45)
   data(PAM50genes)
   data(mb)
-  mb$D7 = (mb$time < 7*365.25) & (mb$cens == 1)
-  mb$D7[(mb$time < 7*365.25) & (mb$cens == 0)] = NA
+  mb$D7 <<- (mb$time < 7*365.25) & (mb$cens == 1)
+  mb$D7[(mb$time < 7*365.25) & (mb$cens == 0)] <<- NA
   attr(mb, 'ySurv') <<- with(data=mb, survival::Surv(time, cens))
   # double-headed <<- is required
 }
