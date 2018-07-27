35e7dc5c48dcc54cdf69aae683766c632cd30197
Roger Day <notsofeo@gmail.com>
2018-07-27 15:59:15 -0400

    Automatically make genepairs

--- Weakest_link/R/onAttach.R
+++ Weakest_link/R/onAttach.R
@@ -1,9 +1,11 @@
 .onAttach = function(libname, pkgname) {
   data(p45)
+  genepairs <<- makeGenePairs()
   data(PAM50genes)
   data(mb)
   mb$D7 <<- (mb$time < 7*365.25) & (mb$cens == 1)
   mb$D7[(mb$time < 7*365.25) & (mb$cens == 0)] <<- NA
   attr(mb, 'ySurv') <<- with(data=mb, survival::Surv(time, cens))
+  D7label = 'Died by 7 years'
   # double-headed <<- is required
 }
